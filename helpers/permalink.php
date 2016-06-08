9f28a8f11be864bacc4dbb3495e828ee4feae3b2
Miika Arponen <miika@geniem.com>
2016-06-08 14:33:07 +0300

    Permalink id parameter

--- helpers/permalink.php
+++ helpers/permalink.php
@@ -1,4 +1,12 @@
 <?php
 $this->dust->helpers['permalink'] = function ( \Dust\Evaluate\Chunk $chunk, \Dust\Evaluate\Context $ctx, \Dust\Evaluate\Bodies $bodies, \Dust\Evaluate\Parameters $params ) {
-	return $chunk->write( get_permalink() );
+
+	if ( isset( $params->id ) ) {
+		$id = $params->id;
+	}
+	else {
+		$id = get_the_ID();
+	}
+
+	return $chunk->write( get_permalink( $id ) );
 };
