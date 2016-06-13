d4da8701f9ae58f074450316dfa14fd67f8e7804
Miika Arponen <miika@geniem.com>
2016-06-13 15:06:30 +0300

    Fixed submenu object name on the menu helper data

--- dustpress.php
+++ dustpress.php
@@ -6,7 +6,7 @@ Description: Dust.js templating system for WordPress
 Author: Miika Arponen & Ville Siltala / Geniem Oy
 Author URI: http://www.geniem.com
 License: GPLv3
-Version: 0.4.0.2
+Version: 0.4.0.4
 */
 
 final class DustPress {
