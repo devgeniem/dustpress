6159c69c9f5ddd19bdcabccd91d59b01f834373f
Miika Arponen <miika@geniem.com>
2016-07-05 15:13:56 +0300

    Minor bug fix to strtodate helper

--- dustpress.php
+++ dustpress.php
@@ -6,7 +6,7 @@ Description: Dust.js templating system for WordPress
 Author: Miika Arponen & Ville Siltala / Geniem Oy
 Author URI: http://www.geniem.com
 License: GPLv3
-Version: 1.0.6
+Version: 1.0.7
 */
 
 final class DustPress {
--- helpers/strtodate.php
+++ helpers/strtodate.php
@@ -3,9 +3,9 @@ namespace DustPress;
 
 class Strtodate extends Helper {
     public function output() {
-		$value 	= $params->value;
-		$format	= $params->format;
-		$now	= $params->now;
+		$value 	= $this->params->value;
+		$format	= $this->params->format;
+		$now	= $this->params->now;
 		
 		return date( $format, strtotime( $value, $now ) );
     }
