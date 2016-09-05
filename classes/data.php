14ea73253e137b873ed0508e21e2838d64be29e2
Miika Arponen <miika@geniem.com>
2016-09-05 10:57:06 +0300

    Fix for Component filter and strings that include a null character

--- classes/data.php
+++ classes/data.php
@@ -2,52 +2,54 @@
 namespace DustPress;
 
 class Data {
-	/**
-	 * A filter to alter the data of an ACF Flexible Content component.
-	 * @param  mixed $data
-	 * @return mixed
-	 */
-	public static function component_invoke( $data ) {
-		self::component_handle( $data );
+    /**
+     * A filter to alter the data of an ACF Flexible Content component.
+     * @param  mixed $data
+     * @return mixed
+     */
+    public static function component_invoke( $data ) {
+        self::component_handle( $data );
 
-		return $data;
-	}
+        return $data;
+    }
 
-	public static function component_handle( &$data, $fields = false ) {
-		if ( is_array( $data ) || is_object( $data ) ) {
-	        foreach ( (array) $data as $key => $item ) {
-	            if ( "fields" == $key ) {
-	                if ( is_array( $item ) && isset( $item["acf_fc_layout"] ) ) {
-	                	if ( is_array( $data ) ) {
-	                    	$data[ $key ] = apply_filters( "dustpress/data/component=" . $item["acf_fc_layout"], $item );
-	                    }
-	                    else {
-	                    	$data->{$key} = apply_filters( "dustpress/data/component=" . $item["acf_fc_layout"], $item );	
-	                    }
-	                }
-	                
-	                if ( is_array( $data ) ) {
-	                	self::component_handle( $data[ $key ], true );
-	                }
-	                else if ( get_class($data) == "stdClass" || get_class($data) == "WP_Post" ) {
-	                	self::component_handle( $data->{$key}, true );
-	                }
-	                else if ( is_object( $data ) ) {
-	                	self::component_handle( $data->{$key}, true );
-	                }
-	            }
-	            else {
-	                if ( is_array( $data ) ) {
-	                	self::component_handle( $data[ $key ], true );
-	                }
-	                else if ( get_class($data) == "stdClass" || get_class($data) == "WP_Post" ) {
-	                	self::component_handle( $data->{$key}, true );
-	                }
-	                else if ( is_object( $data ) ) {
-	                	self::component_handle( $data->{$key}, true );
-	                }
-	            }
-	        }
-	    }
-	}
+    public static function component_handle( &$data, $fields = false ) {
+        if ( is_array( $data ) || is_object( $data ) ) {
+            foreach ( (array) $data as $key => $item ) {
+                if ( ! preg_match( "/\0/", $key ) ) {
+                    if ( "fields" == $key ) {
+                        if ( is_array( $item ) && isset( $item["acf_fc_layout"] ) ) {
+                            if ( is_array( $data ) ) {
+                                $data[ $key ] = apply_filters( "dustpress/data/component=" . $item["acf_fc_layout"], $item );
+                            }
+                            else {
+                                $data->{$key} = apply_filters( "dustpress/data/component=" . $item["acf_fc_layout"], $item );   
+                            }
+                        }
+                        
+                        if ( is_array( $data ) ) {
+                            self::component_handle( $data[ $key ], true );
+                        }
+                        else if ( get_class($data) == "stdClass" || get_class($data) == "WP_Post" ) {
+                            self::component_handle( $data->{$key}, true );
+                        }
+                        else if ( is_object( $data ) ) {
+                            self::component_handle( $data->{$key}, true );
+                        }
+                    }
+                    else {
+                        if ( is_array( $data ) ) {
+                            self::component_handle( $data[ $key ], true );
+                        }
+                        else if ( get_class($data) == "stdClass" || get_class($data) == "WP_Post" ) {
+                            self::component_handle( $data->{$key}, true );
+                        }
+                        else if ( is_object( $data ) ) {
+                            self::component_handle( $data->{$key}, true );
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
--- dustpress.php
+++ dustpress.php
@@ -6,7 +6,7 @@ Description: Dust.js templating system for WordPress
 Author: Miika Arponen & Ville Siltala / Geniem Oy
 Author URI: http://www.geniem.com
 License: GPLv3
-Version: 1.1.17
+Version: 1.1.18
 */
 
 final class DustPress {
