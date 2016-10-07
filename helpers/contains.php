4ded4b94078eb85c1308023ade553faebf1ed02b
Ville Siltala <ville.siltala@geniem.com>
2016-10-07 12:10:00 +0300

    The Contains helper now accepts also objects as the key.

--- helpers/contains.php
+++ helpers/contains.php
@@ -1,16 +1,27 @@
 <?php
+/**
+ * The Contains helper
+ */
 namespace DustPress;
 
-class Contains extends \Dust\Helper\Comparison
-{
-    public function isValid($key, $value) {
-    	if ( is_array( $key ) ) {
-        	return in_array( $value, $key );
-        }
-        else {
-        	return false;
-        }
+/**
+ * This helper extends the DustPHP Comparison class and provides
+ * a functionality to check whether a given value is in an array/object.
+ */
+class Contains extends \Dust\Helper\Comparison {
+
+    /**
+     * Implements the isValid function of the Comparison class.
+     * Checks whether a given value is in an array/object.
+     *
+     * @param  object/array $key   The array/object to look from.
+     * @param  any          $value The value to look for.
+     * @return boolean
+     */
+    public function isValid( $key, $value ) {
+        $haystack = (array) $key;
+        return in_array( $value, $haystack );
     }
 }
 
-$this->add_helper( "contains", new Contains() );
+$this->add_helper( 'contains', new Contains() );
