f7db61c2c091e5d5ca9a6618d4f01ec2fc67f746
Miika Arponen <miika@geniem.com>
2017-01-13 16:38:33 +0200

    Minor bug fix

--- dustpress.php
+++ dustpress.php
@@ -6,7 +6,7 @@ Description: Dust.js templating system for WordPress
 Author: Miika Arponen & Ville Siltala / Geniem Oy
 Author URI: http://www.geniem.com
 License: GPLv3
-Version: 1.5.2
+Version: 1.5.3
 */
 
 final class DustPress {
