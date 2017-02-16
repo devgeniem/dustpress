d303a54bb379db7f7859fa72478ac363a7a7612f
Ville Siltala <ville.siltala@geniem.com>
2017-02-16 13:56:39 +0200

    Changed the 'allowed_functions' property to 'api' and added support for depricated models, update model class code documentation.

--- dustpress.php
+++ dustpress.php
@@ -6,7 +6,7 @@ Description: Dust.js templating system for WordPress
 Author: Miika Arponen & Ville Siltala / Geniem Oy
 Author URI: http://www.geniem.com
 License: GPLv3
-Version: 1.5.4
+Version: 1.5.5
 */
 
 final class DustPress {
