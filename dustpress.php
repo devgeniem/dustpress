1a36bd3ddc7adac04433c43797cb2e5989c73af2
Miika Arponen <miika@geniem.com>
2019-04-08 15:22:24 +0300

    Fixed another bug regarding the custom route render type setting.

--- dustpress.php
+++ dustpress.php
@@ -6,7 +6,7 @@ Description: Dust.js templating system for WordPress
 Author: Miika Arponen & Ville Siltala / Geniem Oy
 Author URI: http://www.geniem.com
 License: GPLv3
-Version: 1.22.1
+Version: 1.22.2
 */
 
 final class DustPress {
