f10fea3ec9707861bd584fb9a0c922ea31801a3a
Miika Arponen <miika@geniem.com>
2019-12-03 10:01:32 +0200

    Version 1.26.3

--- dustpress.php
+++ dustpress.php
@@ -6,7 +6,7 @@ Description: Dust.js templating system for WordPress
 Author: Miika Arponen & Ville Siltala / Geniem Oy
 Author URI: http://www.geniem.com
 License: GPLv3
-Version: 1.26.2
+Version: 1.26.3
 */
 
 final class DustPress {
