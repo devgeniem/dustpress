4b41a7b209dd2f61756fec834b88946d42b919b8
Ismo Vuorinen <ismo.vuorinen@geniem.com>
2020-04-24 16:02:24 +0300

    classes/helper.php
    Documentation, CodeStyle fixes and cleaned up if/else structures.
    
    - First the logic was reversed from dummy check
    - Then as method_exists check for 'prerun' was only thing in block,
      combined those checks into single if statement
    - As chunk->write() returns Chunk, added it to DocBlock
    - Return statement was not filled in the end, so added final return

--- classes/helper.php
+++ classes/helper.php
@@ -1,30 +1,74 @@
 <?php
+/**
+ * This file contains DustPress Helper class.
+ */
+
 namespace DustPress;
 
+/**
+ * Class Helper
+ *
+ * @package DustPress
+ */
 class Helper {
-	protected $chunk;
-	protected $context;
-	protected $bodies;
-	protected $params;
+    /**
+     * Chunk object.
+     *
+     * @var \Dust\Evaluate\Chunk
+     */
+    protected $chunk;
+    /**
+     * Context object.
+     *
+     * @var \Dust\Evaluate\Context
+     */
+    protected $context;
+    /**
+     * Bodies to evaluate.
+     *
+     * @var \Dust\Evaluate\Bodies
+     */
+    protected $bodies;
+    /**
+     * Parameters.
+     *
+     * @var \Dust\Evaluate\Parameters
+     */
+    protected $params;
 
-	public function __invoke(\Dust\Evaluate\Chunk $chunk, \Dust\Evaluate\Context $context, \Dust\Evaluate\Bodies $bodies, \Dust\Evaluate\Parameters $params) {
+    /**
+     * Invoice Helper
+     *
+     * @param \Dust\Evaluate\Chunk      $chunk   Chunk object.
+     * @param \Dust\Evaluate\Context    $context Context object.
+     * @param \Dust\Evaluate\Bodies     $bodies  Bodies to evaluate.
+     * @param \Dust\Evaluate\Parameters $params  Parameters object.
+     *
+     * @return \Dust\Evaluate\Chunk
+     */
+    public function __invoke(
+        \Dust\Evaluate\Chunk $chunk,
+        \Dust\Evaluate\Context $context,
+        \Dust\Evaluate\Bodies $bodies,
+        \Dust\Evaluate\Parameters $params
+    ) {
 
-		$this->chunk = $chunk;
-		$this->context = $context;
-		$this->bodies = $bodies;
-		$this->params = $params;
+        $this->chunk   = $chunk;
+        $this->context = $context;
+        $this->bodies  = $bodies;
+        $this->params  = $params;
 
-		if ( ! isset( $this->bodies->dummy ) ) {
-			if ( method_exists( $this, "init" ) ) {
-				return $this->init();
-			}
-			else if ( method_exists( $this, "output" ) ) {
-				return $this->chunk->write( $this->output() );
-			}
-		} else {
-            if ( method_exists( $this, "prerun" ) ) {
-                $this->prerun();
-            }
+        if ( isset( $this->bodies->dummy ) && method_exists( $this, 'prerun' ) ) {
+            $this->prerun();
         }
-	}
+
+        if ( method_exists( $this, 'init' ) ) {
+            return $this->init();
+        }
+        if ( method_exists( $this, 'output' ) ) {
+            return $this->chunk->write( $this->output() );
+        }
+
+        return $this->chunk;
+    }
 }
