8441fc4555948d6adb16c9846856883c651ade6e
3Dblog maker <narutoshipfan@gmail.com>
2020-12-08 18:15:24 +0200

    Create excerpt.php
    A little bit a monkeying around, there was no {@excerpt/} like there were {@content/} So I figure out to make new copy_paste from the content helper and just change every instance of a the_content()-function on to a the_excerpt()-function and make some fiddling and then name it new file to a excerpt, thus I made that {@excerpt/} works.

---
+++ helpers/excerpt.php
@@ -0,0 +1,23 @@
+namespace DustPress;
+
+class Excerpt extends Helper { 
+    public function output() {
+        global $post;
+
+        if ( isset( $this->params->data ) ) {
+            return apply_filters( 'the_excerpt', $this->params->data );
+        } else {
+            if ( isset( $this->params->id ) ) {
+                $post = get_post( $this->params->id );
+            }
+            
+            ob_start();
+            setup_postdata( $post );
+            the_excerpt();
+            wp_reset_postdata();
+            return ob_get_clean(); 
+        }
+    }
+}
+
+$this->add_helper( 'excerpt', new Excerpt() );
